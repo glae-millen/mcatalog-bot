af0dc97beb1bb5702a02f86214ec755255b7f3ef
glae-millen <alpha.mrf.cm1@gmail.com>
2020-06-16 11:10:39 +0800

    Commit #3.2 - bugfix

--- index.js
+++ index.js
@@ -11,33 +11,33 @@ client.Discord = Discord;
 client.fs = fs;
 client.gSheet = gSheet;
 
-const http = require('http');
-const express = require('express');
-const app = express();
-
-app.get("/", (request, response) =>
-{
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-
 // Glitch stuff
+// const http = require('http');
+// const express = require('express');
+// const app = express();
+//
+// app.get("/", (request, response) =>
+// {
+//   console.log(Date.now() + " Ping Received");
+//   response.sendStatus(200);
+// });
+//
 // app.listen(process.env.PORT);
 // setInterval(() =>
 // {
 //   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 // }, 280000);
-//
-// fs.readdir("./events/", (err, files) =>
-// {
-// 	if (err) return console.error(err);
-// 	files.forEach(file =>
-//   {
-// 		const event = require(`./events/${file}`);
-// 		let eventName = file.split(".")[0];
-// 		client.on(eventName, event.bind(null, client));
-// 	});
-// });
+
+fs.readdir("./events/", (err, files) =>
+{
+	if (err) return console.error(err);
+	files.forEach(file =>
+  {
+		const event = require(`./events/${file}`);
+		let eventName = file.split(".")[0];
+		client.on(eventName, event.bind(null, client));
+	});
+});
 
 client.commands = new Enmap();
 
