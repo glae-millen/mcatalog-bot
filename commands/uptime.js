029d3b7c9f89dcf2eb4b16bdbe2945d50e40c312
glae-millen <alpha.mrf.cm1@gmail.com>
2020-06-18 23:53:36 +0800

    Commit #3.3 - bugfix on 'uptime'

--- commands/uptime.js
+++ commands/uptime.js
@@ -5,6 +5,7 @@ exports.run = (client, message, args) => {
 
 	let totalSeconds = (client.uptime / 1000);
   let days = Math.floor(totalSeconds / 86400);
+  totalSeconds %= 86400;
   let hours = Math.floor(totalSeconds / 3600);
   totalSeconds %= 3600;
   let minutes = Math.floor(totalSeconds / 60);
